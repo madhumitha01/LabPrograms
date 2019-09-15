ec89389aa17b72be370f0f990900e0d95411f3a0
Madhumitha Rajinikanthan <madhumitharajinikanthan@gmail.com>
2019-09-15 22:53:48 +0530

    Added Triangle Testing problem

---
+++ Software Testing/2a_TriangleBVT/src/TriangleTestCase.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TriangleTestCase {
+
+	@Test
+	public void test1() {
+		Triangle t = new Triangle();
+		String s=t.compute(100,100,1);
+		assertEquals("Isosceles Triangle",s);
+	}
+
+}
